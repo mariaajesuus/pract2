12b384fbb2eb66fa8fff6a2c19af7477763e9c83
mariaajesuus <146821293+mariaajesuus@users.noreply.github.com>
2024-09-30 10:46:42 +0200

    Initial commit

---
+++ src/ahorcado.py
@@ -0,0 +1,76 @@
+import random
+
+def cargar_palabras(ruta):
+    '''
+    Recibe la ruta de un fichero de texto que contiene una palabra por línea y devuelve
+    dichas palabras en una lista.
+    '''
+    with open(ruta, encoding='utf-8') as f:
+        res = []
+        for linea in f:
+            res.append(linea.strip()) # strip() elimina los espacios en blanco y saltos de línea al principio y al final
+        return res
+
+def elegir_palabra(palabras):
+    '''
+    Elige la palabra a adivinar:
+    - Selecciona una palabra aleatoria de la lista 'palabras'
+    - Devuelve la palabra seleccionada
+    Ayuda: 
+    - La función 'random.choice' del paquete 'random' recibe una lista de opciones y 
+      devuelve una de ellas seleccionada aleatoriamente.
+    '''
+    return random.choice(palabras)
+    
+def enmascarar_palabra(palabra, letras_probadas):
+    '''
+    Enmascarar la palabra:
+    - Inicializar una lista vacía. 
+    - Recorrer cada letra de la palabra, añadiendola a la lista 
+      si forma parte de las letras_probadas, o añadiendo un '_' en caso contrario. 
+    - Devuelve una cadena concatenando los elementos de la lista (ver 'Ayuda')
+    Ayuda: 
+    - Utilice el método join de las cadenas. Observe el siguiente ejemplo:
+        ' '.join(['a','b','c']) # Devuelve "a b c"
+    '''
+    lista_caracteres = [] # Inicializar una lista vacía
+    for letra in palabra: # Recorrer cada letra de la palabra
+        # Si la letra está en la lista letras_probadas
+        if letra in letras_probadas:                   # ESCRIBIR CONDICIÓN
+            # Añadir a lista_caracteres la letra
+            lista_caracteres.append(letra)       # COMPLETAR
+        else:
+            # Añadir a lista_caracteres un guión bajo '_'
+            lista_caracteres.append('_')       # COMPLETAR
+    
+    return ' '.join(lista_caracteres)
+
+def pedir_letra(letras_probadas):
+    '''
+    Pedir la siguiente letra:
+    - Pedirle al usuario que escriba la siguiente letra por teclado
+    - Comprobar si la letra indicada ya se había propuesto antes y pedir otra si es así
+    - Considerar las letras en minúsculas aunque el usuario las escriba en mayúsculas
+    - Devolver la letra
+    Ayuda:
+    - La función 'input' permite leer una cadena de texto desde la entrada estándar
+    - El método 'lower' aplicado a una cadena devuelve una copia de la cadena en minúsculas
+    '''
+    letra = input("Di una letra:").lower()
+    while letra in letras_probadas:
+        letra = input("Esa ya la dijiste, di otra letra:").lower()
+    return letra
+
+def comprobar_letra(palabra_secreta, letra):
+    '''
+    Comprobar letra:
+    - Comprobar si la letra está en la palabra secreta o no
+    - Mostrar el mensaje correspondiente informando al usuario
+    - Devolver True si estaba y False si no
+    '''
+    if letra in palabra_secreta:
+        print("¡Letra correcta!")
+        return True
+    else:
+        print("Esa letra no es correcta :-(")
+        return False
